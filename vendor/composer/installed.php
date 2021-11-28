f7e47cf7b47a341a3692b03b586e5f50ac98dd88
Joseph Shrock <joseph@shrockservices.com>
2021-11-27 23:33:06 -0500

    install roundcube package

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4a7b2a80b0f415aaf701bfdeb9c170626dcaa467',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4a7b2a80b0f415aaf701bfdeb9c170626dcaa467',
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'pretty_version' => '0.3.1',
+            'version' => '0.3.1.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../roundcube/plugin-installer',
+            'aliases' => array(),
+            'reference' => '9c66c276b795eea1a32eb864b2c6c1f682cdc027',
+            'dev_requirement' => false,
+        ),
+    ),
+);
