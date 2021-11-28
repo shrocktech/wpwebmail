4a7b2a80b0f415aaf701bfdeb9c170626dcaa467
Joseph Shrock <joseph@shrockservices.com>
2021-11-27 23:28:02 -0500

    commit files and folders

---
+++ includes/class-wpwebmail-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/shrocktech
+ * @since      1.0.0
+ *
+ * @package    Wpwebmail
+ * @subpackage Wpwebmail/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wpwebmail
+ * @subpackage Wpwebmail/includes
+ * @author     ShrockTech <shrockservices@gmail.com>
+ */
+class Wpwebmail_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
